f19ebcb52601374f4822532b44e71ae8c80b758d
Arpit Gupta <imarg_3@zoho.com>
2020-08-25 00:41:46 +0530

    Add useLayoutEffect for setting navigation options

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,18 +1,38 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Context } from "../context/BlogContext";
+import { EvilIcons } from "@expo/vector-icons";
 
-const ShowScreen = ({ route }) => {
+const ShowScreen = ({ navigation, route }) => {
   const { state } = useContext(Context);
 
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Edit", { id: route.params.id })}
+        >
+          <EvilIcons name="pencil" style={styles.pencilicon} />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
   const blogPost = state.find((blogPost) => blogPost.id === route.params.id);
+
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  pencilicon: {
+    paddingHorizontal: 10,
+    fontSize: 35,
+  },
+});
 
 export default ShowScreen;
